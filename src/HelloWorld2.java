9ed4247cc014ffa9a0f320ef01f27c5271432333
BIT <BIT@DESKTOP-LRHLNNS>
2019-08-30 13:57:44 +0900

    aa

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -3,6 +3,7 @@ public class HelloWorld2 {
 
 	public static void main(String[] args) {
 		System.out.println("gdfd");
+		System.out.println("dlfs");
 	}
 
 }
---
+++ src/helloWorld/ehldfoa.java
@@ -0,0 +1,7 @@
+package helloWorld;
+
+public class ehldfoa {
+	public static void main(String[] args) {
+		System.out.println("djofa");
+	}
+}
